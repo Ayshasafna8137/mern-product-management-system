fc3d16fb9755e4cb398da09a7e571e3350f893d6
Ayshasafna8137 <ayishasafna02@gmail.com>
2026-03-09 12:46:32 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,18 @@ const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
 
+app.get("/", (req, res) => {
+  res.send("MERN Product Management API Running");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-
 const PORT = process.env.PORT || 5000;
-
 
 mongoose
   .connect(process.env.MONGO_URI)
