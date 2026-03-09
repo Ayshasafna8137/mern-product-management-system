dcb7a101f3fb5d2747371091475a7e12a80bf9e0
Ayshasafna8137 <ayishasafna02@gmail.com>
2026-03-09 11:50:10 +0530

    update server port for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,23 +8,27 @@ const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
+
+const PORT = process.env.PORT || 5000;
+
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-
     console.log("MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
-
   })
   .catch((error) => {
-    console.error(error);
+    console.error("MongoDB connection error:", error);
   });
